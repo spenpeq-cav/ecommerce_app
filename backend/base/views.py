f646ff882332a1366b413bd5f783d3e58a7f27ed
Spencer <spencer.peq@gmail.com>
2021-02-21 13:31:54 -0800

    JSON web token installation, setup, customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,24 @@ from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 
 @api_view(['GET'])
 def getRoutes(request):
