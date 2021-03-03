b3218c0368a2edb4913999492772362a9d18a271
Spencer <spencer.peq@gmail.com>
2021-03-02 20:22:38 -0800

    User Login Reducer and Action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,7 +20,7 @@ export const listProducts = () => async (dispatch) => {
     }catch(error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+            payload: error.response && error.response.data.detail ? error.response.data.detail : error.message,
         })
     }
 }
@@ -39,7 +39,7 @@ export const listProductDetails = (id) => async (dispatch) => {
     }catch(error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+            payload: error.response && error.response.data.detail ? error.response.data.detail : error.message,
         })
     }
 }
