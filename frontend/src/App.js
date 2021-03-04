1741154710e5c3f2439d873c2ca770334fd9f897
Spencer <spencer.peq@gmail.com>
2021-03-04 10:58:21 -0800

    User register reducer, action, and screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Container>
             <Route path ='/' component={HomeScreen} exact />
             <Route path ='/login' component={LoginScreen} />
+            <Route path ='/register' component={RegisterScreen} />
             <Route path ='/product/:id' component={ProductScreen} />
             <Route path ='/cart/:id?' component={CartScreen} />
           </Container>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
     cart: cartReducer,
     userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
