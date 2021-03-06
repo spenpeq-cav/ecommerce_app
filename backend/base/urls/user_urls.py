bd0f09a00da2b06b7b3bbcfb5ee65561955a43a4
Spencer <spencer.peq@gmail.com>
2021-03-05 19:27:37 -0800

    Created Update Profile endpoint, Profile Screen, Get User Details

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register/', views.registerUser, name="register"),
 
     path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="users-profile-update"),
+
     path('', views.getUsers, name="users"),
 ]
