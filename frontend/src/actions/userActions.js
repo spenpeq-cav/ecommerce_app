bb7ab8272bec89ca8cf7c5ffc8926ce062cf9ddc
Spencer <spencer.peq@gmail.com>
2021-03-06 19:43:17 -0800

    Update User Profile and User Details bug fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,6 +13,12 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
+
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async(dispatch) => {
@@ -51,6 +57,7 @@ export const login = (email, password) => async(dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
 }
 
 export const register = (name, email, password) => async(dispatch) => {
@@ -122,6 +129,50 @@ export const getUserDetails = (id) => async(dispatch, getState) => {
     }catch(error) {
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail ? error.response.data.detail : error.message,
+        })
+    }
+}
+
+
+export const updateUserProfile = (user) => async(dispatch, getState) => {
+    try{
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers:{
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(
+            `/api/users/profile/update/`,
+            user,
+            config
+        )
+
+        dispatch({
+            type: USER_UPDATE_PROFILE_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    }catch(error) {
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload: error.response && error.response.data.detail ? error.response.data.detail : error.message,
         })
     }
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -5,7 +5,8 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 
 function ProfileScreen({ history }) {
@@ -24,18 +25,23 @@ function ProfileScreen({ history }) {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
+
+
     useEffect(() => {
         if(!userInfo){
             history.push('/login')
         }else {
-            if(!user || !user.name){
+            if(!user || !user.name || success){
+                dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
             }else {
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -43,7 +49,12 @@ function ProfileScreen({ history }) {
         if(password != confirmPassword){
             setMessage('Passwords do not match!')
         }else{
-            console.log('Updating...')
+            dispatch(updateUserProfile({
+                'id': user._id,
+                'name': name,
+                'email': email,
+                'password': password,
+            }))
         }
         
     }
