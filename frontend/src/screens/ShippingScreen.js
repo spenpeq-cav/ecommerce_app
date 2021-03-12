f3120b9d76d2901b382e80b7cd090bd6b1389d4c
Spencer <spencer.peq@gmail.com>
2021-03-11 22:49:34 -0800

    Checkout steps component

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useState, useEffect} from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import {saveShippingAddress} from '../actions/cartActions'
 
 function ShippingScreen({ history }) {
@@ -25,6 +26,7 @@ function ShippingScreen({ history }) {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 step3 step4/>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
 
