f624f18177d877ccf8a709cbe8288e8070052dd8
Spencer <spencer.peq@gmail.com>
2021-04-06 17:57:53 -0700

    Get Order by Id endpoint

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -62,3 +62,21 @@ def addOrderItems(request):
         serializer = OrderSerializer(order, many=False)
 
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail':'Not authorized to views this order'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail':'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
